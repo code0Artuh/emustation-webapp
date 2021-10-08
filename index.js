3e740e161b05ff77cd5b7d4e323e5690910275a3
Bruno Daniel <bruno.daniel@outlook.com>
2021-10-07 22:49:29 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const  express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
+
+
+
+
+app.listen(port, () => console.log(`Running in http://localhost:${port}`));
