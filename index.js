6f44378e9cc524aa55cf03598012e45ee54f49c5
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-08 01:11:02 -0300

    main

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
+const Game = [{
+    Nome: "The Legend of Zelda",
+    Ano: "1997",
+    Link:"https://myemulator.online/emu?game=MzM2",
+    Img: "/img/zelda.png"
+}]
 
 
-
-
-app.listen(port, () => console.log(`Running in http://localhost:${port}`));
-
-
-  app.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.render("index")
   });
 
-  app.get("/details", (req, res) => {
-    res.render("detalhes");
+  app.get("/details/:id", (req, res) => {
+    const id = req.params.id;
+    const jogo = Game[id];
+    res.render("details", {
+      jogo,
+    });
   });
 
   app.get("/cadastro", (req, res) => {
@@ -26,9 +31,12 @@ app.listen(port, () => console.log(`Running in http://localhost:${port}`));
   });
 
   app.get("/snes", (req, res) => {
-    res.render("snes");
+    res.render("snes",  {Game});
   }); 
 
   app.get("/md", (req, res) => {
     res.render("md");
   });
+
+
+app.listen(port, () => console.log(`Running in http://localhost:${port}`));
