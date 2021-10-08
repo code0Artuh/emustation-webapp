648dd701ca1b3871bd8d57ef666300e231b9a9a7
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-07 23:04:06 -0300

    main

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ app.use(express.urlencoded());
 
 
 app.listen(port, () => console.log(`Running in http://localhost:${port}`));
+
+
+  app.get("/", (req, res) => {
+    res.render("index")
+  });
+
+  app.get("/details", (req, res) => {
+    res.render("detalhes");
+  });
+
+  app.get("/cadastro", (req, res) => {
+    res.render("cadastro");
+  });
+
+  app.get("/snes", (req, res) => {
+    res.render("snes");
+  }); 
+
+  app.get("/md", (req, res) => {
+    res.render("md");
+  });
