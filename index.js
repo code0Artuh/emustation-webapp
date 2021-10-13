f096001fa6df3d7b35cccd36e5e4cfed0cd51e90
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-13 20:52:25 -0300

    main

--- index.js
+++ index.js
@@ -1,96 +1,112 @@
-const  express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-require('dotenv').config();
+require("dotenv").config();
 
 //importando os dois js que estão interligando com o banco de dados
-const database = require('./model/database/index.js');
-const jogos = require('./model/index.js');
+const database = require("./model/database/index.js");
+const jogos = require("./model/index.js");
 
 //funções async trabalhando com promise com o banco de dados
 (async () => {
-    await database.sync();
-    const product = await jogos.findAll();
-    console.log(product);
-
+  await database.sync();
+  const product = await jogos.findAll();
+  console.log(product);
 })();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
-
 app.get("/", (req, res) => {
-    res.render("index")
+  res.render("index");
+});
+
+app.get("/details/:id", async (req, res) => {
+  const id = req.params.id;
+  const jogo = await jogos.findAll({ from: { jogos }, where: { id: id } });
+  res.render("details", { jogo });
+});
+
+app.get("/snes", async (req, res) => {
+  const lista = await jogos.findAll({
+    from: { jogos },
+    where: { cons: "Snes" },
   });
+  res.render("snes", { lista });
+});
 
-  app.get("/details/:id", async (req, res) => {
-    const id = req.params.id;
-    const jogo = await jogos.findAll({from: {jogos}, where:{id:id}});
-    res.render("details", {jogo});
-  });
+app.get("/md", async (req, res) => {
+  const lista = await jogos.findAll({ from: { jogos }, where: { cons: "Md" } });
+  res.render("md", { lista });
+});
 
-  app.get("/snes", async (req, res) => {
-    const lista = await jogos.findAll({from:{jogos},where:{cons:'Snes'}});
-    res.render("snes", {lista});
-  }); 
+app.get("/editar/:id", async (req, res) => {
+  const id = req.params.id;
+  const jogo = await jogos.findAll({ from: { jogos }, where: { id: id } });
+  if (!jogo) {
+    res.render("editar", { mensagem: "jogo não encontrado!" });
+  }
+  res.render("editar", { jogo });
+});
 
-  app.get("/md", async (req, res) => {
-    const lista = await jogos.findAll({from:{jogos},where:{cons:'Md'}});
-    res.render("md", {lista});
-  });
+app.post("/editar/:id", async (req, res) => {
+  const id = req.params.id;
+  const jogo = await jogos.findAll({ from: { jogos }, where: { id: id } });
+  const { nome, descricao, imagem } = req.body;
 
+  jogos.update({
+  nome: nome,
+  descricao: descricao,
+  imagem: imagem},{where:{id: id}});
 
-  //Aqui esta recebendo os cadastros e mandando pro banco de dados
-  app.post("/new", (req, res) => {
-    const {
-      nome,
-      tipo,
-      imagem,
-      descricao,
-      cons
-    } = req.body;
-    if (!nome) {
-      res.render("details", {
-        mensagem: "Nome é obrigatório",
-      });
-    }
-    if (!imagem) {
-      res.render("details", {
-        mensagem: "Imagem é obrigatório",
-      });
-    }
-    if (!tipo) {
-      res.render("details", {
-        mensagem: "Tipo é obrigatório",
-      });
-    }
-    if (!descricao) {
-      res.render("details", {
-        mensagem: "Descrição é obrigatório",
-      });
-    }
-    if (!cons) {
-      res.render("details", {
-        mensagem: "Snes ou Md é obrigatório",
-      });
-    }
-    try {
-      jogos.create({
-        nome:nome,
-        tipo:tipo,
-        descricao:descricao,
-        imagem:imagem,
-        cons:cons});
-        res.redirect("/")
-    } catch (err) {
-      console.log(err);
-      res.render("details", {
-        mensagem: "Ocorreu um erro ao cadastrar o jogo!",
-      });
-    }
-  });
+  res.redirect("/");
+});
 
+//Aqui esta recebendo os cadastros e mandando pro banco de dados
+app.post("/new", (req, res) => {
+  const { nome, tipo, imagem, descricao, cons } = req.body;
+  if (!nome) {
+    res.render("details", {
+      mensagem: "Nome é obrigatório",
+    });
+  }
+  if (!imagem) {
+    res.render("details", {
+      mensagem: "Imagem é obrigatório",
+    });
+  }
+  if (!tipo) {
+    res.render("details", {
+      mensagem: "Tipo é obrigatório",
+    });
+  }
+  if (!descricao) {
+    res.render("details", {
+      mensagem: "Descrição é obrigatório",
+    });
+  }
+  if (!cons) {
+    res.render("details", {
+      mensagem: "Snes ou Md é obrigatório",
+    });
+  }
+  try {
+    jogos.create({
+      nome: nome,
+      tipo: tipo,
+      descricao: descricao,
+      imagem: imagem,
+      cons: cons,
+    });
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.render("details", {
+      mensagem: "Ocorreu um erro ao cadastrar o jogo!",
+    });
+  }
+});
 
 app.listen(port, () => console.log(`Running in http://localhost:${port}`));
