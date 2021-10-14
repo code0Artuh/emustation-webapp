ca3313b50d5d719b6a16587240ae601714c28d4f
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-14 09:53:34 -0300

    main

--- model/database/index.js
+++ model/database/index.js
@@ -3,7 +3,10 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   host:  process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 async function conectado(){
