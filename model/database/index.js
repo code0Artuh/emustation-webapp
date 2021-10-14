1a39594695d4d39a3521c9ab59236fbc89d9b53b
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-14 10:37:57 -0300

    main

--- model/database/index.js
+++ model/database/index.js
@@ -1,12 +1,13 @@
-const { Sequelize } = require("sequelize");
-const connectionString = 'postgres://lubqxysawbdpsj:cc5cb4e46a141af9618437eb8bbcfff3f6877f8643223596989d29cbfc0baf18@ec2-18-215-96-54.compute-1.amazonaws.com:5432/d59sl9b4pa23uj';
+const { Sequelize, DataType } = require("sequelize");
 
 
-const sequelize = new Sequelize({connectionString: connectionString,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect:  'postgres',
+  protocol: 'postgres',
+  logging:  true ,
   ssl: {
     rejectUnauthorized: false
   },
-  dialect: "postgres"
 });
 
 async function conectado(){
