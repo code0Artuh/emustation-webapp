0892d640cb02b6e96e6c85b06a96c69d386d21ac
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-14 19:40:53 -0300

    main

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post("/delete/:id", async (req, res) => {
   const jogo = await jogos.findByPk(req.params.id);
 
   await jogo.destroy();
-  menssage = `O ${jogo.nome} foi deletado com sucesso!`
+  message = `O ${jogo.nome} foi deletado com sucesso!`
 
   res.redirect("/");
 });
