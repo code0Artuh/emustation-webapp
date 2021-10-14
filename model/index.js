071d57b7103afc816d003ace5a795b7e2e639c01
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-14 09:45:41 -0300

    main

--- model/index.js
+++ model/index.js
@@ -1,17 +1,30 @@
-module.exports = (DataTypes , sequelize) => {
-    const jogos = sequelize.define('jogos', {
-      id: {
-        type: DataTypes.INTEGER,
-      },
-      name: {
-        type: DataTypes.STRING,
-      },
-      email: {
-        type: DataTypes.STRING,
-      },
-      lastEmail: {
-        type: DataTypes.TIME,
-      }
-    })
-    return jogos;
-    };
+const { Sequelize, DataTypes } = require('sequelize');
+const database = require('./database');
+
+const jogos = database.sequelize.define("jogos", {
+    ID: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    Nome:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    Descriçao:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    Setor:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    }
+},{
+    freezeTableName: true,
+    timestamps: false, 
+    createdAt: false,
+    updatedAt: false,
+})
+
+module.exports = jogos;
