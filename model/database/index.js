3d211c00729b332cddbca8ef92851b0c324fc9e1
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-14 05:55:26 -0300

    main

--- model/database/index.js
+++ model/database/index.js
@@ -8,14 +8,5 @@ const sequelize = new Sequelize({
   }
 });
 
-sequelize.connect();
-
-sequelize.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  sequelize.end();
-});
 
 module.exports = sequelize;
