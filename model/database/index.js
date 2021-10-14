c98957beab55d3832c3ad881a86aab60c443f3c9
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-14 10:58:40 -0300

    main

--- model/database/index.js
+++ model/database/index.js
@@ -1,9 +1,9 @@
-const { Sequelize, DataType } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect:  'postgres',
-  protocol: 'postgres',
-  logging:  true ,
+const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
+  host:  process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres'
 });
 
 async function conectado(){
