3723d999b2b141b1c959a779c32968bf46e6bec9
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-14 03:13:38 -0300

    main

--- model/database/index.js
+++ model/database/index.js
@@ -1,9 +1,8 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
-  host:  process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres'
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('d59sl9b4pa23uj', 'lubqxysawbdpsj', 'cc5cb4e46a141af9618437eb8bbcfff3f6877f8643223596989d29cbfc0baf18', {
+    dialect: 'postgres',
+    host: 'ec2-18-215-96-54.compute-1.amazonaws.com',
+    port: 5432
 });
 
 module.exports = sequelize;
