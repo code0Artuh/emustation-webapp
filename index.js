af36f582b417d305c3f1992ff0f21af0b4a7a785
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-10-14 04:58:57 -0300

    main

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-require("dotenv").config();
+
 
 let message = "";
 
